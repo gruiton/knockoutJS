6924ea8767573c327220fb010d5f0b1a0e270202
gruiton <julen.ormaetxea81@gmail.com>
2016-04-02 21:52:53 +0200

    final app

--- goaltracker/app.js
+++ goaltracker/app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongojs = require('mongojs');
-var db = mongojs('goaltracker', ['goals']);
+var db = mongojs('goaltracker',['goals']);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')));
@@ -12,59 +12,55 @@ app.get('/', function(req, res){
 	res.send('It Works!');
 });
 
-app.get('/goals', function(req, res){	
+app.get('/goals', function(req, res){
 	db.goals.find(function(err, docs){
 		if(err){
 			res.send(err);
-		} else{
+		} else {
 			console.log('Getting Goals...');
 			res.json(docs);
 		}
 	});
 });
 
+
 app.post('/goals', function(req, res){
-	db.goals.insert(req.body, function(err, docs){
+	db.goals.insert(req.body, function(err, doc){
 		if(err){
 			res.send(err);
-		} else{
+		} else {
 			console.log('Adding Goals...');
-			res.json(docs);
+			res.json(doc);
 		}
 	});
 });
 
-app.post('/goals/:id', function(req, res){
-	db.goals.findAndModify({
-		query: {_id: mongojs.ObjectId(req.params.id)}, 
-		update: { 
-			$set: {
-				name: req.body.name,
-				type: req.body.type,
-				deadline: req.body.deadline
-			}
-		},
-		new: true
-	},
-	function(err, docs){
+
+app.put('/goals/:id', function(req, res){
+	db.goals.findAndModify({query:{_id: mongojs.ObjectId(req.params.id)},
+	update:{ $set:{
+		name: req.body.name,
+		type: req.body.type,
+		deadline: req.body.deadline
+	}},
+	new: true
+	 }, function(err, doc){
 		if(err){
-			console.log("update: " + err);
 			res.send(err);
-		} else{
+		} else {
 			console.log('Updating Goals...');
-			res.json(docs);
+			res.json(doc);
 		}
-	})
+	});
 });
 
-app.post('/goals/:id', function(req, res){
-	db.goals.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, docs){
+app.delete('/goals/:id', function(req, res){
+	db.goals.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, doc){
 		if(err){
-			console.log("delete: " + err);
 			res.send(err);
-		} else{
+		} else {
 			console.log('Removing Goals...');
-			res.json(docs);
+			res.json(doc);
 		}
 	});
 });
